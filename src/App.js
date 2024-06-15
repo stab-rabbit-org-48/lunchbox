66521388766731d2880ae36ccc9d8626cb861a23
Ian Mann <ianmann86@gmail.com>
2024-06-14 17:04:23 -0700

    main commit test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+const App = () => {
+return (
+	<div>
+	<h1 className="heading">GeeksForGeeks</h1>
+	<h4 className="sub-heading">
+		A computer science portal for geeks
+	</h4>
+	</div>
+);
+};
+
+export default App;
