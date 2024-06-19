650c08a1c4e7cfc381500b31892f081651b51094
Lauren <laurenfelty01@gmail.com>
2024-06-18 19:29:29 -0500

    add naviagtion bar test and protein button test

--- __tests__/HomePage.js
+++ __tests__/HomePage.js
@@ -18,7 +18,7 @@ describe('Home page renders correctly', () => {
         <Home />
       </BrowserRouter>
     );
-    const pickProtein = screen.getAllByText(/pick your protein/i);
+    const pickProtein = screen.getByText(/choose your protein/i);
     const nutritionButton = screen.getByRole('button', { name: /Nutrition/i });
     const recipeButton = screen.getByRole('button', { name: /recipe/i });
     const timerButton = screen.getByRole('button', { name: /timer/i });
@@ -41,5 +41,49 @@ describe('Home page renders correctly', () => {
     expect(turkeyButton).toBeInTheDocument();
     expect(fishButton).toBeInTheDocument();
     expect(vegButton).toBeInTheDocument();
+  });
+
+  test('Navigation bar buttons take user to correct location', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const nutritionButton = screen.getByRole('button', { name: /Nutrition/i });
+    const recipeButton = screen.getByRole('button', { name: /recipe/i });
+    const timerButton = screen.getByRole('button', { name: /timer/i });
+    const accountButton = screen.getByRole('button', { name: /account/i });
+
+    fireEvent.click(nutritionButton);
+    expect(nutritionButton.nextElementSibling);
+    fireEvent.click(recipeButton);
+    expect(recipeButton.nextElementSibling);
+    fireEvent.click(timerButton);
+    expect(timerButton.nextElementSibling);
+    fireEvent.click(accountButton);
+    expect(accountButton.nextElementSibling);
+  });
+  test('Different protein buttons take user to correct ', () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    );
+    const beefButton = screen.getByRole('button', { name: /beef/i });
+    const chickenButton = screen.getByRole('button', { name: /chicken/i });
+    const turkeyButton = screen.getByRole('button', { name: /turkey/i });
+    const fishButton = screen.getByRole('button', { name: /fish/i });
+    const vegButton = screen.getByRole('button', { name: /Vegetarian/i });
+
+    fireEvent.click(beefButton);
+    expect(beefButton.nextElementSibling);
+    fireEvent.click(chickenButton);
+    expect(chickenButton.nextElementSibling);
+    fireEvent.click(turkeyButton);
+    expect(turkeyButton.nextElementSibling);
+    fireEvent.click(fishButton);
+    expect(fishButton.nextElementSibling);
+    fireEvent.click(vegButton);
+    expect(vegButton.nextElementSibling);
   });
 });
